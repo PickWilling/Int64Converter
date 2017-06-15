31253d43bc340f4db5a4cdcb4e7ef4c1831f3e7f
PickWilling <PickWilling@163.com>
2017-06-15 19:29:16 +0800

    增加程序图标

--- Int64/Properties/Resources.Designer.cs
+++ Int64/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Int64Converter.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   查找类似于 (Icon) 的 System.Drawing.Icon 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Icon _121_upper_Phi {
+            get {
+                object obj = ResourceManager.GetObject("_121_upper_Phi", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
