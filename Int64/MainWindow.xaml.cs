ad595ac591dc0905911100223ec5a01c647519cd
PickWilling <PickWilling@163.com>
2017-06-15 19:13:15 +0800

    基本完成

--- Int64/MainWindow.xaml.cs
+++ Int64/MainWindow.xaml.cs
@@ -24,12 +24,34 @@ namespace Int64Converter
         }
 
         //检测粘贴
-        private void CheckPasting(object sender, DataObjectPastingEventArgs e)
+        private void Check64Pasting(object sender, DataObjectPastingEventArgs e)
         {
             if (e.DataObject.GetDataPresent(typeof(String)))
             {
                 String text = (String)e.DataObject.GetData(typeof(String));
-                if (!isNumberic(text))
+                if (!isNumberic(text) || !IsInt64Num(text))
+                { e.CancelCommand(); }
+            }
+            else { e.CancelCommand(); }
+        }
+
+        private void Check32Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!isNumberic(text) || !IsInt32Num(text))
+                { e.CancelCommand(); }
+            }
+            else { e.CancelCommand(); }
+        }
+
+        private void Check8Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!isNumberic(text) || !IsInt8Num(text))
                 { e.CancelCommand(); }
             }
             else { e.CancelCommand(); }
@@ -42,12 +64,32 @@ namespace Int64Converter
             if (e.Key == Key.Space)
                 e.Handled = true;
         }
+        
 
-
-
-        private void CheckPreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void Check64PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!isNumberic(e.Text))
+            TextBox stTextBox = sender as TextBox;
+            if ((!isNumberic(e.Text) || !IsInt64Num(stTextBox.Text)) && (stTextBox.SelectedText.Length == 0))
+            {
+                e.Handled = true;
+            }
+            else
+                e.Handled = false;
+        }
+        private void Check32PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox stTextBox = sender as TextBox;
+            if ((!isNumberic(e.Text) || !IsInt32Num(stTextBox.Text)) && (stTextBox.SelectedText.Length == 0))
+            {
+                e.Handled = true;
+            }
+            else
+                e.Handled = false;
+        }
+        private void Check8PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox stTextBox = sender as TextBox;
+            if ((!isNumberic(e.Text) || !IsInt8Num(stTextBox.Text)) && (stTextBox.SelectedText.Length == 0))
             {
                 e.Handled = true;
             }
@@ -66,6 +108,41 @@ namespace Int64Converter
                 if (!char.IsDigit(c))
                     //if(c<'0' c="">'9')//最好的方法,在下面测试数据中再加一个0，然后这种方法效率会搞10毫秒左右
                     return false;
+            }
+            return true;
+        }
+
+        public static bool IsInt64Num(string strTest)
+        {
+            Int64 i64 = 0;
+            if (strTest.Length > 0 && !Int64.TryParse(strTest, out i64))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsInt32Num(string strTest)
+        {
+            Int32 i32 = 0;
+            if (strTest.Length > 0 && !Int32.TryParse(strTest, out i32))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsInt8Num(string strTest)
+        {
+            const Int32 iMax = 0xFF;
+            Int32 i32 = 0;
+            if (strTest.Length > 0 && !Int32.TryParse(strTest, out i32))
+            {
+                return false;
+            }
+            if (i32 > iMax)
+            {
+                return false;
             }
             return true;
         }
