0b4655efd2805f3078e4b534d90d0c62e4b1f472
PickWilling <PickWilling@163.com>
2017-06-15 10:35:14 +0800

    可以校验数据的正确性

--- Int64/CInt64ToH32Converter.cs
+++ Int64/CInt64ToH32Converter.cs
@@ -11,7 +11,11 @@ namespace Int64Converter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            Int64 iInput = Int64.Parse(value.ToString());
+            Int64 iInput;
+            if (!Int64.TryParse(value.ToString(), out iInput))
+            {
+                return "0";
+            }
             Int64 iOut = iInput >> 32;
             return iOut.ToString();
             //string strInt64 = value as string;
@@ -21,7 +25,11 @@ namespace Int64Converter
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            Int32 iInput = Int32.Parse(value.ToString());
+            Int32 iInput;
+            if (!Int32.TryParse(value.ToString(), out iInput))
+            {
+                return "0";
+            }
             return iInput.ToString();
             //return value;
         }
---
+++ Int64/NumCheckRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Int64Converter
+{
+    class NumCheckRule : ValidationRule
+    {
+        private int min;
+        private int max;
+
+        public int Min { get{return min;} set{min = value; } }
+        public int Max { get { return max; } set { max = value; } }
+
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            Int64 iNum;
+            if (!Int64.TryParse(value.ToString(), out iNum))
+            {
+                return new ValidationResult(false, "Invalid number format");
+            }
+            //if (iNum < min || iNum > max)
+            //{
+            //    return new ValidationResult(false, string.Format("Out of range({0}-{1})", min, max));
+            //}
+            return ValidationResult.ValidResult;
+        }
+    }
+}
