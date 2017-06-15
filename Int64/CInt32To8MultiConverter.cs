522ed785acc4d475e67c8049aafd9910192972a5
PickWilling <PickWilling@163.com>
2017-06-15 17:12:03 +0800

    基本完成

---
+++ Int64/CInt32To8MultiConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+
+namespace Int64Converter
+{
+    class CInt32To8MultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Int32 iHH8Input = 0;
+            if (!Int32.TryParse(values[0].ToString(), out iHH8Input))
+            {
+                return "0";
+            }
+            Int32 iHL8Input = 0;
+            if (!Int32.TryParse(values[1].ToString(), out iHL8Input))
+            {
+                return "0";
+            }
+            Int32 iLH8Input = 0;
+            if (!Int32.TryParse(values[2].ToString(), out iLH8Input))
+            {
+                return "0";
+            }
+            Int32 iLL8Input = 0;
+            if (!Int32.TryParse(values[3].ToString(), out iLL8Input))
+            {
+                return "0";
+            }
+            Int32 iOut = (iHH8Input << 24) | (iHL8Input << 16) | (iLH8Input << 8) | iLL8Input;
+            return iOut.ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string[] result = { "0", "0", "0", "0" };
+            Int32 i32Input = 0;
+            if (!Int32.TryParse(value.ToString(), out i32Input))
+            {
+                return result;
+            }
+            Int32 iOutTemp = i32Input >> 24;
+            result[0] = iOutTemp.ToString();
+            iOutTemp = i32Input >> 16;
+            result[1] = iOutTemp.ToString();
+            iOutTemp = i32Input >> 8;
+            result[2] = iOutTemp.ToString();
+            iOutTemp = i32Input & 0xFFFF;
+            result[3] = iOutTemp.ToString();
+            return result;
+        }
+    }
+}
---
+++ Int64/CInt64MultiConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+
+namespace Int64Converter
+{
+    class CInt64To32MultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Int32 iH32Input = 0;
+            if (!Int32.TryParse(values[0].ToString(), out iH32Input))
+            {
+                return "0";
+            }
+            Int32 iL32Input = 0;
+            if (!Int32.TryParse(values[1].ToString(), out iL32Input))
+            {
+                return "0";
+            }
+            Int64 iTemp = iL32Input;
+            Int64 iOut = iH32Input;
+            iOut = (iOut << 32) | iTemp;
+            return iOut.ToString();
+
+            //return "1";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string[] result = {"0", "0"};
+            Int64 i64Input = 0;
+            if (!Int64.TryParse(value.ToString(), out i64Input))
+            {
+                return result;
+            }
+            Int64 iOutTemp = i64Input >> 32;
+            result[0] = iOutTemp.ToString();
+            iOutTemp = i64Input & 0xFFFFFFFF;
+            result[1] = iOutTemp.ToString();
+            return result;
+        }
+    }
+}
